95b219d360499c091d0f00c404a437938f973a4e
kyosuke.yasue <futarigoto.y.k@gmail.com>
2020-09-27 01:21:12 +0000

    kadai-tasklist

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -9,7 +9,8 @@
             <thead>
                 <tr>
                     <th style="width:10%">id</th>
-                    <th style="width:90%">タスク</th>
+                    <th style="width:70%">タスク</th>
+                    <th style="width:20%">ステータス</th>
                 </tr>
             </thead>
             <tbody>
@@ -17,6 +18,7 @@
                 <tr>
                     <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
                     <td>{{ $task->content }}</td>
+                    <td>{{ $task->status }}</td>
                 </tr>
                 @endforeach
             </tbody>
